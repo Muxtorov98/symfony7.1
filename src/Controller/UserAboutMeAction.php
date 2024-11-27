28cbf51a1116cf6cb199d38dde979c573d099541
Tulqin_Muxtorov98 <104984928+Muxtorov98@users.noreply.github.com>
2024-11-27 10:58:55 +0500

    first commit

---
+++ src/Controller/UserAboutMeAction.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Controller\Base\AbstractController;
+use App\Entity\User;
+use App\Repository\UserRepository;
+
+class UserAboutMeAction extends AbstractController
+{
+    public function __invoke(UserRepository $userRepository): ?User
+    {
+        return $this->getUser();
+    }
+}
---
+++ src/Controller/UserCreateAction.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Component\User\UserFactory;
+use App\Component\User\UserManager;
+use App\Controller\Base\AbstractController;
+use App\Entity\User;
+
+/**
+ * Class CreateUserController
+ *
+ * @package App\Controller
+ */
+class UserCreateAction extends AbstractController
+{
+    public function __invoke(User $data, UserFactory $userFactory, UserManager $userManager): User
+    {
+        print_r(11);
+        exit();
+        $this->validate($data);
+
+        $user = $userFactory->create($data->getEmail(), $data->getPassword());
+        $userManager->save($user, true);
+
+        return $user;
+    }
+}
